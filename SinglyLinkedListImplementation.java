036475adf6b09799bc7d95e2650c4d24992e3363
Vincent Munene <munenevincent49@gmail.com>
2021-07-11 17:16:37 +0300

    implemented the Sinlgy-linked list

--- SinglyLinkedListImplementation.java
+++ SinglyLinkedListImplementation.java
@@ -1,1 +1,68 @@
 package projects;
+
+class SinglyLinkedList {
+    public static void main(String[] args) {
+        SinglyList list = new SinglyList();
+        list.addFirst(3);
+        list.addFirst(5);
+        list.addFirst(4);
+        list.addFirst(2);
+        list.addLast(9);
+        list.addLast(10);
+        list.deleteFirst();
+
+        list.display();
+
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+}
+
+class SinglyList {
+    private Node first;
+
+    SinglyList() {
+        first = null;
+
+    }
+
+    public boolean isEmpty() {
+        return (first == null);
+    }
+
+    public void addFirst(int data) {
+        Node newNode = new Node();
+        newNode.data = data;
+        newNode.next = first;
+        first = newNode;
+    }
+
+    public void addLast(int data) {
+        Node current = first;
+        while (current.next != null) {
+            current = current.next;
+        }
+        Node newNode = new Node();
+        newNode.data = data;
+        current.next = newNode;
+
+    }
+
+    public Node deleteFirst() {
+        Node current = first;
+        first = first.next;
+
+        return current;
+    }
+
+    public void display() {
+        Node current = first;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+}
