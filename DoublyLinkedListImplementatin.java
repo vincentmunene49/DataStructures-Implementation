e4ee4dfa0b9b6f2e921b98672101583ce7abe5c7
Vincent Munene <munenevincent49@gmail.com>
2021-07-12 19:23:24 +0300

    implemented Doubly Linked List

--- DoublyLinkedListImplementatin.java
+++ DoublyLinkedListImplementatin.java
@@ -1,1 +1,154 @@
 package projects;
+
+class DoublyLinkedListImplementation {
+    public static void main(String[] args) {
+
+        DoubleImplementation list = new DoubleImplementation();
+        list.addFirst(0);
+        // list.insertAfter(1, 2);
+        list.addFirst(1);
+        list.addFirst(2);
+        list.addFirst(3);
+        list.addLast(4);
+        list.addLast(5);
+        list.deleteFirst();
+        list.deleteLast();
+        list.insertAfter(2, 7);
+        list.deleteKey(1);
+
+        list.display();
+
+    }
+}
+
+class ANode {
+    int data;
+    ANode next;
+    ANode previous;
+}
+
+class DoubleImplementation {
+    private ANode first;
+    private ANode last;
+
+    DoubleImplementation() {
+        first = null;
+        last = null;
+    }
+
+    public void addFirst(int data) {
+        ANode newNode = new ANode();
+        newNode.data = data;
+        if (isEmpty()) {
+            last = newNode;
+        } else {
+            first.previous = newNode;
+        }
+
+        newNode.next = first;
+        first = newNode;
+
+    }
+
+    public void addLast(int data) {
+        ANode newNode = new ANode();
+        newNode.data = data;
+        if (isEmpty()) {
+            first = newNode;
+        } else {
+            last.next = newNode;
+            newNode.previous = last;
+        }
+
+        last = newNode;
+    }
+
+    public ANode deleteFirst() {
+        ANode current = first;
+        if (first.next == null) {
+            last = null;
+        } else {
+            first.next.previous = null;
+        }
+        first = first.next;
+        return current;
+
+    }
+
+    public ANode deleteLast() {
+        ANode currentLast = last;
+        if (last == null) {
+            first = null;
+        } else {
+            last.previous.next = null;
+        }
+        last = last.previous;
+
+        return currentLast;
+    }
+
+    public boolean insertAfter(int value, int data) {
+        ANode current = first;
+
+        while (current.data != value) {
+            current = current.next;
+
+            if (current == null) {
+                return false;
+            }
+        }
+        ANode newNode = new ANode();
+        newNode.data = data;
+
+        if (current == last) {
+            current.next = null;
+            last = newNode;
+        } else {
+            current.next.previous = newNode;
+            newNode.next = current.next;
+
+        }
+        newNode.previous = current;
+        current.next = newNode;
+        return true;
+
+    }
+
+    public ANode deleteKey(int key) {
+        ANode current = first;
+
+        while (current.data != key) {
+            current = current.next;
+            if (current == null) {
+                return null;
+            }
+
+        }
+        // check if it is the last
+        if (current == last) {
+            current.previous.next = null;
+            last = current.previous;
+        } else if (current == first) {
+            current.next.previous = null;
+            first = current.next;
+        } else {
+            current.previous.next = current.next;
+            current.next.previous = current.previous;
+        }
+
+        return current;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void display() {
+        ANode current = first;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+
+}
